7ae49bb6a32990798be6c0bd23aa6ee3210bb2db
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-08-07 07:40:36 -0400

    Initial Commit: Modify .gitignore

---
+++ src/main/java/net/saddlercoms/radiodensity/RadiodensityApplication.java
@@ -0,0 +1,13 @@
+package net.saddlercoms.radiodensity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiodensityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiodensityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/saddlercoms/radiodensity/RadiodensityApplicationTests.java
@@ -0,0 +1,13 @@
+package net.saddlercoms.radiodensity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiodensityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
