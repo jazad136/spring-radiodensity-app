7a0bd8c4d4defdac07437cfc60373e0e8d563e80
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-10-09 07:08:30 -0400

    Add Nearby Table Schema, Upd RStation Schema

--- src/main/resources/db/migration/V1.1__AddRStationTable.sql
+++ src/main/resources/db/migration/V1.1__AddRStationTable.sql
@@ -1,7 +1,7 @@
 CREATE SEQUENCE rs_rstation_seq start with 1;
 CREATE TABLE rstation (
 	radio_id bigint NOT NULL DEFAULT nextval('rs_rstation_seq'),
-	call_name varchar(40), 
+	call_name varchar(40) UNIQUE, 
 	fm_freq numeric(4,1) NOT NULL,
 	category varchar(40), 
 	haat int, 
