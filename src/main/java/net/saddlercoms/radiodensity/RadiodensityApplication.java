52fd79c44fbd66cf9608ee63b49c744cc53f9b77
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-10-15 20:38:24 -0400

    Add CommandLineRunner Test to application

--- src/main/java/net/saddlercoms/radiodensity/RadiodensityApplication.java
+++ src/main/java/net/saddlercoms/radiodensity/RadiodensityApplication.java
@@ -1,13 +1,31 @@
 package net.saddlercoms.radiodensity;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net.saddlercoms.radiodensity.db.dao.RStationDAO;
+import net.saddlercoms.radiodensity.db.model.RStation;
+
 @SpringBootApplication
-public class RadiodensityApplication {
+public class RadiodensityApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RadiodensityApplication.class, args);
 	}
 
+	
+	@Autowired RStationDAO rsDAO;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		List<RStation> allStation = rsDAO.findAll();
+		System.out.println("** FIRST 10 STATIONS **");
+		allStation.stream().limit(10).forEach(System.out::println);
+	}
+
+	
 }
